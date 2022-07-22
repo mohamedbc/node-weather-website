9d7c88d64a4254c27baed4ec1fa25e24e02d7cc5
mohamedbc <babchikh.mohamed@gmail.com>
2022-07-22 10:35:04 +0100

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs = require('hbs')
 
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const indexURL = path.join(__dirname, '../public')
 const viewsURL = path.join(__dirname, '../templates/views')
